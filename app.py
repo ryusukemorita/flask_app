362d344b1562a37cefe4dc0ac3889fd542d4c29c
Ryusuke_Morita <r.frtun702@gmail.com>
2020-08-12 10:21:29 +0900

    2020/08/12

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -7,6 +7,13 @@ def index():
     return render_template('index.html',
                             title="index with Jinja",
                             message = "test")
+
+@app.route('/<id>/<password>')
+def index2(id, password):
+    msg = 'id: %s, password: %s' % (id, password)
+    return render_template('index.html',
+                            title="index with Jinja",
+                            message = msg)
 
 if __name__ == '__main__':
     app.debug = True
