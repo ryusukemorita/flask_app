9d7e9704a37eb2f373a99b7b1a5ad36ad1433b19
Ryusuke_Morita <r.frtun702@gmail.com>
2020-08-12 10:42:35 +0900

    2020/08/12 Jinja_form

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods = ['GET'])
 def index():
     return render_template('index.html',
-                            title="index with Jinja",
-                            message = "test")
+                            title = "index with Jinja",
+                            message = "What's your name?")
 
-@app.route('/<id>/<password>')
-def index2(id, password):
-    msg = 'id: %s, password: %s' % (id, password)
+@app.route('/', methods = ['POST'])
+def form():
+    field = request.form['field']
     return render_template('index.html',
                             title="index with Jinja",
-                            message = msg)
+                            message = "Hi, %s" % field)
 
 if __name__ == '__main__':
     app.debug = True
